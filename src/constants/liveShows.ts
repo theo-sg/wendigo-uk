23d372986baf726223f3bd8ef64d23e55b26cdb4
theo-sg <theosg94@gmail.com>
2026-05-28 19:58:42 +0100

    [-] remove show

--- src/constants/liveShows.ts
+++ src/constants/liveShows.ts
@@ -10,15 +10,15 @@ export type LiveShow = {
 }
 
 export const LIVE_SHOWS: LiveShow[] = [
-  {
-    id: 'victoria-dalston-2026-04-24',
-    dayLabel: '24',
-    monthLabel: 'Apr',
-    venueLabel: 'The Victoria Dalston',
-    cityLabel: 'London',
-    countryLabel: 'UK',
-    lineup: ['Laid Off', 'Jan Echo', 'Dude, My Dude', 'WENDIGO'],
-    ticketUrl:
-      'https://www.bandsintown.com/e/108209732-wendigo-at-the-victoria-dalston?came_from=251&utm_medium=web&utm_source=artist_page&utm_campaign=event',
-  },
+//  {
+//    id: 'victoria-dalston-2026-04-24',
+//    dayLabel: '24',
+//    monthLabel: 'Apr',
+//    venueLabel: 'The Victoria Dalston',
+//    cityLabel: 'London',
+//    countryLabel: 'UK',
+//    lineup: ['Laid Off', 'Jan Echo', 'Dude, My Dude', 'WENDIGO'],
+//    ticketUrl:
+//      'https://www.bandsintown.com/e/108209732-wendigo-at-the-victoria-dalston?came_from=251&utm_medium=web&utm_source=artist_page&utm_campaign=event',
+//  },
 ]
