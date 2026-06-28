ae80c2af690905d74c74a14d7b80f6589af948a2
theo-sg <theosg94@gmail.com>
2026-06-28 21:44:07 +0100

    [-] TOWER

--- src/components/home/NewSongSection.tsx
+++ src/components/home/NewSongSection.tsx
@@ -1,6 +1,8 @@
-import xxiiImage from '../../assets/S0119128.JPEG'
-import { EXTERNAL_LINKS } from '../../constants/links'
+import towerImage from '../../assets/many hands tower.jpg'
 import SafeIframe from '../common/SafeIframe'
+
+const TOWER_PRESAVE_URL = 'https://ffm.to/zok6aym'
+const TOWER_INSTAGRAM_EMBED_URL = 'https://www.instagram.com/p/DZ44boPNHZY/embed'
 
 export default function NewSongSection() {
   return (
@@ -9,46 +11,33 @@ export default function NewSongSection() {
         <div className="container padding-top">
           <div className="new-song-visual">
             <img
-              alt="Wendigo promo portrait in dark tones."
+              alt="Wendigo Tower promo artwork."
               className="new-song-photo"
               loading="lazy"
-              src={xxiiImage}
+              src={towerImage}
             />
             <h2 className="new-song-title">
-              <span className="new-song-title-main">xxii</span>
-              <span className="new-song-title-sub">is nothing sacred anymore?</span>
+              <span className="new-song-title-main">tower</span>
             </h2>
-            <div className="embed-frame-wrap new-song-embed-wrap">
-              <SafeIframe
-                className="spotify-embed spotify-embed-compact"
-                src={EXTERNAL_LINKS.tracks.xxii.spotifyEmbed}
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                title="Wendigo latest single on Spotify"
-              />
-            </div>
 
-            <div className="embed-frame-wrap new-song-youtube-wrap">
-              <div className="youtube-embed-wrap">
+            <a
+              className="new-song-presave-link"
+              href={TOWER_PRESAVE_URL}
+              rel="noreferrer"
+              target="_blank"
+            >
+              out june 30 - presave here
+            </a>
+
+            <div className="embed-frame-wrap new-song-instagram-wrap">
+              <div className="instagram-embed-wrap">
                 <SafeIframe
-                  className="youtube-embed"
-                  src={EXTERNAL_LINKS.tracks.xxii.youtubeEmbed}
-                  title="Wendigo video preview"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowFullScreen
+                  className="instagram-embed"
+                  src={TOWER_INSTAGRAM_EMBED_URL}
+                  title="Wendigo Tower Instagram post"
+                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 />
               </div>
-            </div>
-          </div>
-
-          <div className="new-song-video-embed">
-            <div className="youtube-embed-wrap">
-              <SafeIframe
-                className="youtube-embed"
-                src={EXTERNAL_LINKS.tracks.krakensbane.youtubeLiveEmbed}
-                title="KRAKENSBANE LIVE AT CAMDEN ASSEMBLY"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              />
             </div>
           </div>
         </div>
