9672345c461a3ce4e3aedde0c3d6deefe759b916
theo-sg <theosg94@gmail.com>
2026-07-27 23:03:29 +0100

    [-]

--- src/pages/PressKitPage.tsx
+++ src/pages/PressKitPage.tsx
@@ -53,7 +53,7 @@ export default function PressKitPage() {
           </div>
 
           <p className="press-kit-copy">
-            wendigo started as a solo project by singer/guitarist and songwriter<b>theo stewart griffiths</b>, who has played across the uk in many bands on many instruments, from underground club shows, to regional tours. it has now evolved into a full band 
+            wendigo started as a solo project by singer/guitarist and songwriter <b>theo stewart griffiths</b>, who has played across the uk in many bands on many instruments, from underground club shows, to regional tours. it has now evolved into a full band 
             with the addition of bassist <b>toby hawkings</b>, an active member of the west sussex arts, film, and music scene, and drummer <b>rhys caswell</b>, who works as a FOH engineer and audio technician, in addition to touring the uk with various bands. the three of us are united by a shared aesthetic vision for wendigo, and a desire to create music that pushes boundaries and connects with listeners on a deep and visceral level.
           </p>
 
